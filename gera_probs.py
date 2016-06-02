27388f802943c9ff016300147052c3f798ed3497
felipefer42 <felipefer42@gmail.com>
2016-06-02 20:02:14 -0300

    Update gera_probs.py
    tornando o codigo mais compreensivel

--- gera_probs.py
+++ gera_probs.py
@@ -1,24 +1,46 @@
+# Importing libraries
 from utils import *
 from scipy.interpolate import interp1d
 
 def get_name_iso(base_name,age,metal):
+	"""
+	returns the name of an isochrone file for a given age and metallicity
+	
+	:param base_name: isochronal base name
+	:type  base_name: string
+	:param       age: isochronal age in yr
+	:type        age: float
+	:param     metal: isochronal metallicity
+	:type      metal: float
+	:return		: isochronal name
+	:rtype		: string
+	"""
 	return(base_name+'_Z{0}_t{1}e9.dat'.format(metal, age/1e9))
 		
 def get_name_field(base_name,l,b):
+	"""
+	returns the name of a field file for given latitude and longitude
+	
+	:param base_name: field base name
+	:type  base_name: string
+	:param         l: galactic longitude in degrees
+	:type	       l: float
+	:param         b: galactic latitude in degrees
+	:type	       b: float
+	:return	        : field name
+	:rtype          : string
+	"""
 	return(base_name+'_l{0}_b{1}.dat'.format(l, b))
-
 
 ##################
 # Parameters  Iso#
 ############################################
 
-
 ages = [8e9]#,9e9,10e9]
-metal =[0.01]#,0.02,0.03]
+metal = [0.01]#,0.02,0.03]
 d_seq = [20000,60000]
-path = '/home/hperottoni/Documentos/Trabalhos_andamento/caca_fantasmas/Dwarf_test/teste_lembrar/new_version/'
-base_name = '\\teste5'
-
+path = '/'
+base_name = 'isoc'
 
 #####################
 # Parameters  fields#
@@ -26,13 +48,58 @@ base_name = '\\teste5'
 
 gal_l = [90]
 gal_b = [40]
-#gal_b = [40,41]
-path_field = '/home/hperottoni/Documentos/Trabalhos_andamento/caca_fantasmas/Dwarf_test/teste_lembrar/new_version/'
+path_field = '/'
 base_name_field = 'field'
 
+def get_probs(isoc_path, isoc_base_name, isoc_ages, isoc_metal, d_seq, field_path, field_base_name, gal_l, gal_b, save_path):
+    """
+    For each star in each field, calculate the probability of the star belonging to each simple stellar population defined 
+    by each age, metallicity and distance given. Saves one file for each ssp containning the obtained probabilities for all
+    stars.
+    
+    :param       isoc_path: path to the isochrone files
+    :param  isoc_base_name: isochronal base name
+    :param       isoc_ages: list of all ages to be considered. Ages in yr.
+    :param      isoc_metal: list of all metallicities (Z) to be considered.
+    :param           d_seq: list of all distances to be considered. Distances in parsecs.
+    :param      field_path: path to the field files
+    :param field_base_name: field base name
+    :param           gal_l: list of galactic longitudes to be considered.
+    :param           gal_b: list of galactic latitudes to be considered.
+    :param       save_path: path to where result files are to be saved
+    
+    :type        isoc_path: string
+    :type   isoc_base_name: string
+    :type        isoc_ages: list of floats
+    :type       isoc_metal: list of floats
+    :type            d_seq: list of floats
+    :type       field_path: string
+    :type  field_base_name: string
+    :type            gal_l: list of floats
+    :type            gal_b: list of floats
+    :type        save_path: string
+    """
+    
+    for lon in gal_l: # for each given longitude
+        for lat in gal_b: # for each given latitude
+            
+            # Load field data
+            field_name = get_name_field(field_base_name, l = lon, b = lat)
+            # TODO: dar um jeito de permitir que os argumentos de np.loadtxt sejam passados nos argumentos da funcao
+            field_data = np.loadtxt(field_name, delimiter = ",", skiprows = 1)
+            
+            obj_lon = field_data[:,1]
+            obj_lat = field_data[:,2]
+            obj_mag_g = field_data[:,3]
+            obj_mag_r = field_data[:,4]
+            obj_mag_i = field_data[:,5]
+            obj_mag_g_err = field_data[:,6]
+            obj_mag_r_err = field_data[:,7]
+            obj_mag_i_err = field_data[:,8]
+            
 for l in gal_l:
-	
-	for b in gal_b:
+
+    for b in gal_b:
 
 		field_name = 'field0.dat' 
 		data = np.loadtxt(field_name, delimiter = ',',skiprows=1)
